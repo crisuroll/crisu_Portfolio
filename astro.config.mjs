59530036e7b9aae82f8b68d674ee368139d9c9a2
cris <cristina.cg9@hotmail.com>
2025-02-26 20:04:20 +0100

    Fixed pages to deploy website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://crisuroll.github.io/crisu_Portfolio/", // URL de GitHub Pages
+  base: "/crisu_Portfolio/", // Nombre del repositorio
+  output: "static"
 });
