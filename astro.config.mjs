c5c45a519d3598df50dae566a54fb2ef1d52d117
Crisu <cristina.cg9@hotmail.com>
2025-11-23 15:36:32 +0100

    Misc commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://crisuroll.github.io/crisu_Portfolio/", // URL de GitHub Pages
-  base: "/crisu_Portfolio/", // Nombre del repositorio
-  output: "static"
+  base: "/",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
